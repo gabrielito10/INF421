2fc4d8c753151dfe8ad6a35212d10ff15e05330c
gabrielito10 <j.gabito@gmail.com>
2020-08-11 09:42:29 -0400

    Update JuegoDeLeds.ino

--- JuegoDeLeds/JuegoDeLeds.ino
+++ JuegoDeLeds/JuegoDeLeds.ino
@@ -1,9 +1,30 @@
 void setup() {
   // put your setup code here, to run once:
-
+  for (int i=2 ; i<=9 ; i++)
+    pinMode(i,OUTPUT);
+  /*pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT);
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+  pinMode(9,OUTPUT);*/
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (int i=2 ; i<=9 ; i++)
+    digitalWrite(i,HIGH);
+  delay(1000); //pausa en milisegundos    
+  for (int i=2 ; i<=9 ; i++)
+    digitalWrite(i,LOW);
+  delay(1000); //pausa en milisegundos  
+  /*digitalWrite(2,HIGH);
+  digitalWrite(3,HIGH);
+  digitalWrite(4,HIGH);
+  digitalWrite(5,HIGH);
+  digitalWrite(6,HIGH);
+  digitalWrite(7,HIGH);
+  digitalWrite(8,HIGH);
+  digitalWrite(9,HIGH);*/  
 }
