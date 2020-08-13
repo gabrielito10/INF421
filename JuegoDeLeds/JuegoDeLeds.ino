df15f2a375b35ef598b11ddcf1cdc7184d18d83a
gabrielito10 <j.gabito@gmail.com>
2020-08-13 14:26:10 -0400

    INF421

--- JuegoDeLeds/JuegoDeLeds.ino
+++ JuegoDeLeds/JuegoDeLeds.ino
@@ -2,29 +2,21 @@ void setup() {
   // put your setup code here, to run once:
   for (int i=2 ; i<=9 ; i++)
     pinMode(i,OUTPUT);
-  /*pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-  pinMode(4,OUTPUT);
-  pinMode(5,OUTPUT);
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);*/
 }
 
 void loop() {
   for (int i=2 ; i<=9 ; i++)
+  {
     digitalWrite(i,HIGH);
-  delay(1000); //pausa en milisegundos    
-  for (int i=2 ; i<=9 ; i++)
+    delay(50);
     digitalWrite(i,LOW);
-  delay(1000); //pausa en milisegundos  
-  /*digitalWrite(2,HIGH);
-  digitalWrite(3,HIGH);
-  digitalWrite(4,HIGH);
-  digitalWrite(5,HIGH);
-  digitalWrite(6,HIGH);
-  digitalWrite(7,HIGH);
-  digitalWrite(8,HIGH);
-  digitalWrite(9,HIGH);*/  
+    delay(50);
+  }
+  for (int i=8 ; i>=3 ; i--)
+  {
+    digitalWrite(i,HIGH);
+    delay(50);
+    digitalWrite(i,LOW);
+    delay(50);
+  }
 }
