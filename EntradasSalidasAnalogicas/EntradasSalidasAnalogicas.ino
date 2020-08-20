b1053a32772f524fccc11f9871042c5ab1728803
gabrielito10 <j.gabito@gmail.com>
2020-08-20 14:44:45 -0400

    INF435

--- EntradasSalidasAnalogicas/EntradasSalidasAnalogicas.ino
+++ EntradasSalidasAnalogicas/EntradasSalidasAnalogicas.ino
@@ -5,9 +5,9 @@ void setup() {
 
 void loop() {
    int valorAnalogico = analogRead(A0);
-   int nuevoValor = (valorAnalogico * 255)/1023; //conversion de 1023 a 255
+   //int nuevoValor = (valorAnalogico * 255)/1023; //conversion de 1023 a 255
    //conversion a traves del comando map
    //map(valor a convertir,MinEntrada,MaxEntrada,MinSalida,MaxSalida);
    int NuevoValorMAP = map(valorAnalogico,0,1023,0,255);
-   analogWrite(11,nuevoValor);
+   analogWrite(11,NuevoValorMAP);
 }
