1df72b7455a0ac69cdf7c42bae3d74c7740f7286
Gabriel Ibañez <j.gabito.ic@gmail.com>
2021-05-13 14:45:53 -0400

    INF421

--- 05AutoFantasitico/05AutoFantasitico.ino
+++ 05AutoFantasitico/05AutoFantasitico.ino
@@ -1,9 +1,21 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  for (int i=2 ; i<=9 ; i++)
+      pinMode(i,OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for (  int i=2 ; i <=9 ; i++)
+  {
+    digitalWrite(i,HIGH);
+    delay(100);
+    digitalWrite(i,LOW);
+    delay(100);
+  }
+  for (  int i=8 ; i >=3 ; i--)
+  {
+    digitalWrite(i,HIGH);
+    delay(20);
+    digitalWrite(i,LOW);
+    delay(20);
+  }
 }
