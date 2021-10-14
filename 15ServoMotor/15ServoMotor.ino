b74a4bb77be6158b69fe3b44d709faa13d0a600e
gabrielito10 <j.gabito@gmail.com>
2021-10-14 15:06:32 -0400

    INF421

---
+++ 15ServoMotor/15ServoMotor.ino
@@ -0,0 +1,19 @@
+#include <Servo.h>
+Servo miServito;
+void setup() {
+  miServito.attach(3);
+  miServito.write(90);
+  pinMode(12,INPUT);
+  pinMode(11,INPUT);
+  pinMode(10,INPUT);
+}
+
+void loop() {
+  if (digitalRead(12) == HIGH)
+    miServito.write(0);
+  if (digitalRead(11) == HIGH)
+    miServito.write(90);
+  if (digitalRead(10) == HIGH)
+    miServito.write(180);
+  delay(200);
+}
