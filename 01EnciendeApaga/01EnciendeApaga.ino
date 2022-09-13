7c0e19c85e788b907b750ec6d33fa6101a298d6e
gabrielito10 <j.gabito@gmail.com>
2022-09-13 09:07:01 -0400

    INF421

---
+++ 01EnciendeApaga/01EnciendeApaga.ino
@@ -0,0 +1,14 @@
+void setup() {
+  for (int i=2 ; i<=9 ; i++)
+    pinMode(i,OUTPUT);
+}
+
+void loop() {
+  for (int i=2 ; i<=9 ; i++)
+    digitalWrite(i,HIGH);
+  delay(1000);
+
+   for (int i=2 ; i<=9 ; i++)
+    digitalWrite(i,LOW);
+  delay(1000);
+}
