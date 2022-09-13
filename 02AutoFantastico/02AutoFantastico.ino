1d8a20554fa0f8d265e2e046da79d5efa5138dd7
gabrielito10 <j.gabito@gmail.com>
2022-09-13 09:25:07 -0400

    INF421

--- 02AutoFantastico/02AutoFantastico.ino
+++ 02AutoFantastico/02AutoFantastico.ino
@@ -11,7 +11,17 @@ void setup() {
 }
 
 void loop() {
+  //Animando de izq a der
   for (int i=2 ; i<=9 ; i++)
+  {
+    digitalWrite(i,HIGH);
+    delay(pausa);
+    digitalWrite(i,LOW);
+    delay(pausa);
+  }
+
+  //Animando de der o izq
+  for ( int i=8 ; i>=3 ; i--)
   {
     digitalWrite(i,HIGH);
     delay(pausa);
