29426327c115650342eb7519b32b73c19ad78e00
gabrielito10 <j.gabito@gmail.com>
2022-09-13 09:15:24 -0400

    INF421

---
+++ 02AutoFantastico/02AutoFantastico.ino
@@ -0,0 +1,15 @@
+void setup() {
+  //Colocando los pines de 2-9 como salida
+  for (int i=2 ; i<=9 ; i++)
+    pinMode(i,OUTPUT);
+
+  //Colocando los pines 10,11,12 como entrada
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+  pinMode(12,INPUT);  
+}
+
+void loop() {
+  for (int i=2 ; i<=9 ; i++)
+    digitalWrite(i,HIGH);
+}
