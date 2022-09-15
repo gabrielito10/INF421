99f827056f320a28d8f4e190ce65dec8df487fcb
gabrielito10 <j.gabito@gmail.com>
2022-09-15 14:34:15 -0400

    INF421

---
+++ 04PuertoSerieCaracter/04PuertoSerieCaracter.ino
@@ -0,0 +1,24 @@
+void setup() {
+  //Colocando los pines de 2-9 como salida
+  for (int i=2 ; i<=9 ; i++)
+    pinMode(i,OUTPUT);
+
+  //Colocando los pines 10,11,12 como entrada
+  pinMode(10,INPUT);
+  pinMode(11,INPUT);
+  pinMode(12,INPUT);  
+}
+
+void loop() {
+  //Verificando Pulsador pin 10
+  if ( digitalRead(10) == LOW )
+  {
+    digitalWrite(2,HIGH);
+    digitalWrite(3,HIGH);  
+  }
+  else
+  {
+    digitalWrite(2,LOW);
+    digitalWrite(3,LOW);  
+  }
+}
