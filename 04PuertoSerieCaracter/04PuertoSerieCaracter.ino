c89f470140543b5682f49346df055c127fb0b777
gabrielito10 <j.gabito@gmail.com>
2022-09-15 14:46:25 -0400

    INF421

--- 04PuertoSerieCaracter/04PuertoSerieCaracter.ino
+++ 04PuertoSerieCaracter/04PuertoSerieCaracter.ino
@@ -2,23 +2,38 @@ void setup() {
   //Colocando los pines de 2-9 como salida
   for (int i=2 ; i<=9 ; i++)
     pinMode(i,OUTPUT);
-
-  //Colocando los pines 10,11,12 como entrada
-  pinMode(10,INPUT);
-  pinMode(11,INPUT);
-  pinMode(12,INPUT);  
+  //Inicializando Puerto Serie
+  Serial.begin(9600);  
 }
 
 void loop() {
-  //Verificando Pulsador pin 10
-  if ( digitalRead(10) == LOW )
+  if (Serial.available())
   {
-    digitalWrite(2,HIGH);
-    digitalWrite(3,HIGH);  
-  }
-  else
-  {
-    digitalWrite(2,LOW);
-    digitalWrite(3,LOW);  
+    char charRecibido = Serial.read();
+    if ( charRecibido == 'r' || charRecibido == 'R')
+    {
+      digitalWrite(2,HIGH);
+      digitalWrite(3,HIGH);
+    }
+    if ( charRecibido == 'a' || charRecibido == 'A')
+    {
+      digitalWrite(4,HIGH);
+      digitalWrite(5,HIGH);
+    }
+    if ( charRecibido == 'v' || charRecibido == 'V')
+    {
+      digitalWrite(6,HIGH);
+      digitalWrite(7,HIGH);
+    }
+    if ( charRecibido == 'z' || charRecibido == 'Z')
+    {
+      digitalWrite(8,HIGH);
+      digitalWrite(9,HIGH);
+    }
+    if ( charRecibido == 'b' || charRecibido == 'B')
+    {
+      for(int i=2 ; i<=9 ; i++)
+        digitalWrite(i,LOW);
+    }
   }
 }
