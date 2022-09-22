a8d789d632f19d14e37b2684ebda912f2f9d3249
gabrielito10 <j.gabito@gmail.com>
2022-09-22 15:48:22 -0400

    INF421

--- 11RelePulsador/11RelePulsador.ino
+++ 11RelePulsador/11RelePulsador.ino
@@ -1,12 +1,8 @@
 void setup() {
-  //Iniciando Puerto Serie
-  Serial.begin(9600);
+  pinMode(4,OUTPUT);
+  pinMode(10,INPUT);
+  digitalWrite(4,HIGH);
 }
 void loop() {
-  int valorLeido = analogRead(A0);
-  int valorConvertido = map(valorLeido, 0, 1023, 100, 0);
-  Serial.print("Luminosidad -> ");
-  Serial.print(valorConvertido);
-  Serial.println(" %");
-  delay(250);
+  
 }
